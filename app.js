5c0d5dc16986e2c9119d7239e4f5698083c366a0
Divyangana <divyangana97@gmail.com>
2021-09-28 20:26:43 +0530

    Added style and serverURL

---
+++ app.js
@@ -0,0 +1,28 @@
+var txtInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var outputDiv = document.querySelector("#output-box");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text){
+    return serverURL + "?" +"text="+text;
+}
+
+// function errorHandler(error){
+//     console.log("Error is "+error);
+// }
+
+function clickHandler(){
+
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json=>{
+            var outputText = json.contents.translated;
+            outputDiv.innerText = outputText;
+        })
+        // .then(errorHandler);
+};
+
+btnTranslate.addEventListener("click",clickHandler);
